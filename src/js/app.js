d0ea88ce0b40d9581416902b5c57f66a071bdae8
Mark O'Shea <mark@osheatech.com>
2017-02-23 11:32:53 +0000

    initial boiler plate for js app

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Menu from './components/global/menu.js';
+
+ReactDOM.render(
+  <Menu />,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: __dirname + '/src',
+
+  entry: [
+    './js/app.js'
+  ],
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['react-hot-loader', 'babel-loader'], include: path.join(__dirname, 'src'),
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({ template: 'index.html' })
+  ],
+
+  devServer: {
+    hot: true
+  }
+};
