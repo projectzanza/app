86eb8ca78ac0d3390416f2c95f8ff299f8d2e939
Mark O'Shea <mark@osheatech.com>
2017-03-29 16:06:48 +0100

    store the user session in localStorage to persist between refreshes

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -6,13 +6,20 @@ import createLogger from 'redux-logger';
 import { Provider } from 'react-redux';
 import routes from './routes';
 import rootReducer from './reducers';
+import { loadState, saveState } from './localStorage';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 const logger = createLogger();
+const persistedState = loadState();
 const store = createStore(
   rootReducer,
+  persistedState,
   applyMiddleware(thunkMiddleware, logger),
 );
+
+store.subscribe(() => {
+  saveState({ user: store.getState().user });
+});
 
 ReactDOM.render(
   <Provider store={store}>
